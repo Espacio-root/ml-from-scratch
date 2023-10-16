5ff372328f14d495874cc57cd8c35f599a42821a
Espacio-root <Abdullahd00124@gmail.com>
2023-10-17 02:45:39 +0530

    Minor update to file names

--- decision-tree/sample.py
+++ decision-tree/sample.py
@@ -1,4 +1,4 @@
-from main import DecisionTreeClassifier
+from model import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
