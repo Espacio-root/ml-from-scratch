db4dd51f866acd224ad1282e8f0e30401f4babd6
Espacio-root <Abdullahd00124@gmail.com>
2023-10-17 16:07:30 +0530

    Added titanic.csv file into data directory

--- decision-tree/sample.py
+++ decision-tree/sample.py
@@ -3,10 +3,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
-df = pd.read_csv('../data/iris.csv')
-X, y = df.iloc[:, :-1], df.iloc[:, -1]
-for i, v in enumerate(y.unique()):
-    y = y.replace(v, i)
+def process_df(df):
+    df['Sex'] = df['Sex'].replace('male', 0).replace('female', 1)
+    df.dropna(subset=['Embarked', 'Fare'], inplace=True)
+    df['Age'].fillna(-1, inplace=True)
+    for i, v in enumerate(df['Embarked'].unique()):
+        df['Embarked'] = df['Embarked'].replace(v, i)
+
+    columns = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+    return df[columns], df['Survived']
+
+df = pd.read_csv('../data/titanic.csv')
+X, y = process_df(df)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=42)
 
 clf = DecisionTreeClassifier(max_depth=7)
@@ -14,3 +22,4 @@ clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
 print(f'{accuracy_score(y_pred, y_test):.2f}')
+
