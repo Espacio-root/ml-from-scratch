20bf337522bbff9dfd9f93bb20a137863aaad533
Espacio-root <Abdullahd00124@gmail.com>
2023-10-19 01:56:53 +0530

    Added experience-salary linear regression dataset

--- gradient-descent/linear_regression.py
+++ gradient-descent/linear_regression.py
@@ -2,31 +2,53 @@ import numpy as np
 
 class LinearRegression:
 
-    def __init__(self, lr=0.002):
+    def __init__(self, lr=0.001):
         self.lr = lr
 
-        self.slope = 0.0
-        self.intercept = 0.0
+        self.w = 0.0
+        self.b = 0.0
+
+        self.w_history = []
+        self.b_history = []
 
     def forward(self, x):
-        return self.slope * x + self.intercept
+        return self.w * x + self.b
 
     def loss(self, X, Y):
-        return sum((self.forward(X) - Y) ** 2)
+        return np.mean((Y - self.forward(X)) ** 2)
 
     def step(self, X,  Y):
-        grad_slope = sum(2 * (self.forward(X) - Y) * X)
-        grad_intercept = sum(2 * (self.forward(X) - Y))
-
-        self.slope -= self.lr * grad_slope
-        self.intercept -= self.lr * grad_intercept
+        n = len(X)
+        grad_w = (-2/n) * np.sum(X * (Y - self.forward(X)))
+        grad_b = (-2/n) * np.sum(Y - self.forward(X))
+        
+        # Update the parameters using the gradients
+        self.w -= self.lr * grad_w
+        self.b -= self.lr * grad_b
 
         return self.loss(X, Y)
         
-    def fit(self, X, Y, epochs=100):
+    def fit(self, X, Y, epochs=1000, test_size=0.2, early_stopping=100):
         X, Y = np.array(X), np.array(Y)
-        for epoch in range(epochs):
-            loss = self.step(X, Y)
-            print("Epoch: {}, Loss: {}".format(epoch, loss))
+        s = int(len(X) * (1 - test_size))
+        X_train, Y_train, X_test, Y_test = X[:s], Y[:s], X[s:], Y[s:]
+        test_losses = []
+        get_loss_idx = lambda: test_losses.index(min(test_losses))
 
-        return loss
+        for epoch in range(epochs):
+            loss = self.step(X_train, Y_train)
+            test_loss = self.loss(X_test, Y_test)
+            test_losses.append(test_loss)
+            self.w_history.append(self.w)
+            self.b_history.append(self.b)
+            if get_loss_idx() < len(test_losses) - early_stopping:
+                print('Early stopping')
+                break
+            if (epoch+1) % (epochs // 10) == 0:
+                print("Epoch: {}, Loss: {}".format(epoch+1, loss))
+
+        loss_idx = get_loss_idx()
+        return self.w_history[loss_idx], self.b_history[loss_idx]
+
+    def predict(self, X):
+        return np.array([self.forward(x) for x in X])
--- gradient-descent/sample.py
+++ gradient-descent/sample.py
@@ -1,14 +1,22 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
 from linear_regression import LinearRegression
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-X = np.linspace(0, 10, 10)
-y = np.linspace(0, 20, 10)
+df = pd.read_csv('../data/experience-salary.csv')
+X, y = df.iloc[:, -1], df.iloc[:, :-1]
+y = np.squeeze(y)
+print(X.shape, y.shape)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 reg = LinearRegression()
-loss = reg.fit(X, y)
-slope, intercept = reg.slope, reg.intercept
+slope, intercept = reg.fit(X_train, y_train, epochs=10000, test_size=0.2, early_stopping=100)
+
+y_pred = reg.predict(X_test)
+print("Loss: {}".format(reg.loss(X_test, y_test)))
+print("Slope: {}, Intercept: {}".format(slope, intercept))
 
 plt.scatter(X, y)
 plt.plot(X, slope * X + intercept, color='red')
