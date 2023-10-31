6f3c758c4f4dfa9fbe2eec1c6a55b6972f491b29
Espacio-root <Abdullahd00124@gmail.com>
2023-10-31 15:48:57 +0530

    Finished neural network

--- neural-network/neuralnet.py
+++ neural-network/neuralnet.py
@@ -1,50 +1,129 @@
 import numpy as np
 
 
-class Layer:
-    def __init__(self, inp, out, act="relu"):
-        self.w = 0.01 * np.random.randn(inp, out)
-        self.b = np.zeros((1, out))
-        self.act = getattr(self, act)
+class ActLinear:
+    def __call__(self, X):
+        return X
 
-    def forward(self, X):
-        return self.act(X @ self.w + self.b)
+    def backward(self, dZ):
+        return dZ
 
-    def backward(self, y):
-        pass
 
-    def relu(self, X):
+class ActRelu:
+    def __call__(self, X):
+        self.X = X
         return np.maximum(0, X)
 
-    def sigmoid(self, X):
-        e_x = np.exp(X - np.max(X))
-        return e_x / np.sum(e_x, axis=1, keepdims=True)
+    def backward(self, dZ):
+        dZ[self.X <= 0] = 0
+        return dZ
 
 
-class Net:
-    def __init__(self, config):
-        self.layers = [Layer(*config[i : i + 2]) for i in range(len(config) - 2)]
-        self.layers.append(Layer(*config[-2:], act="sigmoid"))
+class ActSoftmax:
+    def __call__(self, X):
+        e_x = np.exp(X - np.max(X, axis=1, keepdims=True))
+        self.output = e_x / np.sum(e_x, axis=1, keepdims=True)
+        return self.output
+
+    def backward(self, dZ):
+        self.dinputs = np.empty_like(dZ)
+        for i, (output, dZ_row) in enumerate(zip(self.output, dZ)):
+            output = output.reshape(-1, 1)
+            jacobian = np.diagflat(output) - np.dot(output, output.T)
+            self.dinputs[i] = np.dot(jacobian, dZ_row)
+        return self.dinputs
+
+class CategoricalCrossEntropy:
+    def __init__(self, net=None, epsilon=1e-7):
+        self.epsilon = epsilon
+        self.net = net
+
+    def __call__(self, y_pred, y_true):
+        y_pred = np.clip(y_pred, self.epsilon, 1-self.epsilon)
+        if len(y_true.shape) == 1:
+            y_true = np.eye(y_pred.shape[1])[y_true]
+        self.y_pred, self.y_true = y_pred, y_true
+        loss = np.sum(y_pred * y_true, axis=1)
+
+        return -np.log(loss).mean()
+
+
+    def backward(self):
+        samples = len(self.y_pred)
+        dZ = -self.y_true / self.y_pred
+        dZ /= samples
+
+        if self.net == None:
+            return dZ
+        self.net.backward(dZ)
+
+
+class CategoricalCrossEntropyWithSoftmax:
+
+    def __init__(self, net=None, epsilon=1e-7):
+        self.epsilon = epsilon
+        self.net = net
+        self.activation = ActSoftmax()
+        self.lossfn = CategoricalCrossEntropy()
+
+    def __call__(self, sig_inputs, y_true):
+        self.outputs, self.y_true  = self.activation(sig_inputs), y_true
+        return self.lossfn(self.outputs, y_true)
+
+    def backward(self):
+        samples = len(self.outputs)
+        if len(self.y_true.shape) == 2:
+            self.y_true = np.argmax(self.y_true, axis=1)
+        dZ = self.outputs.copy()
+        dZ[range(samples), self.y_true] -= 1
+        dZ /= samples
+
+        if self.net == None:
+            return dZ
+        self.net.backward(dZ)
+
+
+class Layer:
+    def __init__(self, inp, out, step_size, act):
+        self.w = 0.01 * np.random.randn(inp, out)
+        self.b = np.zeros((1, out))
+        self.act = act
+        self.step_size = step_size
 
     def forward(self, X):
+        self.X = X
+        return self.act(X @ self.w + self.b)
+
+    def backward(self, dZ):
+        dA = self.act.backward(dZ)
+        dw = self.X.T @ dA
+        db = np.sum(dA, axis=0, keepdims=True)
+
+        self.w -= self.step_size * dw
+        self.b -= self.step_size * db
+
+        return dA @ self.w.T
+
+class Net:
+    def __init__(self, config, step_size=0.01, softmax=False):
+        self.step_size = step_size
+        self.layers = [Layer(*config[i : i + 2], step_size=step_size, act=ActRelu()) for i in range(len(config) - 2)]
+        last_layer_act = ActSigmoid() if softmax else ActLinear()
+        self.layers.append(Layer(*config[-2:], step_size=step_size, act=last_layer_act))
+
+
+    def __call__(self, X):
         for layer in self.layers:
             X = layer.forward(X)
         return X
 
+    def update_step_size(self, new_step_size):
+        self.step_size = new_step_size
+        for layer in self.layers:
+            layer.step_size = new_step_size
 
-class BinaryCrossEntropy:
-    def __init__(self, epsilon=1e-7):
-        self.epsilon = epsilon
+    def backward(self, dZ):
+        for layer in reversed(self.layers):
+            dZ = layer.backward(dZ)
 
-    def __call__(self, y_pred, y):
-        samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, self.epsilon, 1 - self.epsilon)
 
-        if len(y.shape) == 1:
-            confidences = y_pred_clipped[range(samples), y]
-        elif len(y.shape) == 2:
-            confidences = np.sum(y_pred_clipped * y, axis=1)
-        else:
-            raise Exception("y is invalid.")
-
-        return np.mean(-np.log(confidences))
--- neural-network/sample.py
+++ neural-network/sample.py
@@ -1,20 +1,39 @@
-from neuralnet import Net, BinaryCrossEntropy
+from neuralnet import Net, CategoricalCrossEntropyWithSoftmax
 from nnfs.datasets import spiral_data
 import nnfs
 import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import time
 
 nnfs.init()
 X, y = spiral_data(samples=100, classes=3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
 
 # input layer: 2 neurons
 # 1st hidden layer: 32 neurons
 # 2nd hidden layer: 64 neurons
 # output layer: 3 neurons
 layer_config = [2, 32, 64, 3] 
-net = Net(layer_config)
-loss = BinaryCrossEntropy()
+net = Net(layer_config, step_size=0.01)
+loss = CategoricalCrossEntropyWithSoftmax(net)
 
-outputs = net.forward(X)
-loss = loss(outputs, y)
-print(loss)
-
+for epoch in range(100000):
+    outputs = net(X_train)
+    cur_loss = loss(outputs, y_train)
+    loss.backward()
+    if epoch % 100 == 0:
+        print(f'epoch: {epoch}, loss: {cur_loss}')
+    # if epoch % 10 == 0:
+    #     y_pred = net(X_test)
+    #     test_loss = loss(y_pred, y_test)
+    #     if test_loss < 1.029 and net.step_size == 0.1:
+    #         net.update_step_size(net.step_size / 10)
+    #     if test_loss < 0.89 and net.step_size == 0.01:
+    #         net.update_step_size(net.step_size / 10)
+    #     # print([f'{e}: {c}' for e,c in zip(*np.unique(y_test, return_counts=True))])
+    #     acc = accuracy_score(y_pred.argmax(axis=1), y_test)
+    #     print(f"epoch: {epoch}, loss: {test_loss}, accuracy: {acc}")
+    # # if (epoch+1) % 1000 == 0:
+    # #     net.update_step_size(net.step_size / 10)
+    # #     print(f'New step size: {net.step_size}')
